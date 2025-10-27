a0e330fed8131925c387d228e2053d0c7c6ac001
Supersonic Seven <b.rockholz@icloud.com>
2025-10-27 15:32:27 -0400

    Update LaitelaTab.vue
    EZ

--- src/components/tabs/celestial-laitela/LaitelaTab.vue
+++ src/components/tabs/celestial-laitela/LaitelaTab.vue
@@ -59,8 +59,8 @@ export default {
       this.singularityPanelVisible = Currency.singularities.gt(0);
       this.singularitiesUnlocked = Singularity.capIsReached || this.singularityPanelVisible;
       this.singularityCap = Singularity.cap;
-      this.singularityWaitTime = TimeSpan.fromSeconds((this.singularityCap - this.darkEnergy) /
-        Currency.darkEnergy.productionPerSecond).toStringShort();
+      this.singularityWaitTime = TimeSpan.fromSeconds(new Decimal((this.singularityCap - this.darkEnergy) /
+        Currency.darkEnergy.productionPerSecond)).toStringShort();
       this.showAnnihilation = Laitela.annihilationUnlocked;
 
       const d1 = DarkMatterDimension(1);
