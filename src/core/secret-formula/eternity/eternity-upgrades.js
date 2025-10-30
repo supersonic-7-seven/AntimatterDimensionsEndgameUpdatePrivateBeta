784938784f847488b7352aefa85805fce2f1c4b0
Supersonic Seven <b.rockholz@icloud.com>
2025-10-30 16:58:38 -0400

    Update eternity-upgrades.js
    The end?

--- src/core/secret-formula/eternity/eternity-upgrades.js
+++ src/core/secret-formula/eternity/eternity-upgrades.js
@@ -34,7 +34,7 @@ export const eternityUpgrades = {
     description: "Infinity Dimension multiplier based on sum of Infinity Challenge times",
     // The cap limits this at a lower value, but we also need an explicit cap here because very old versions have
     // allowed EC12 to make all the challenge records sum to zero (causing a division by zero here)
-    effect: () => DC.D2.pow(30 / Math.clampMin(Time.infinityChallengeSum.totalSeconds, 0.1)),
+    effect: () => DC.D2.pow(new Decimal(30).div(Decimal.clampMin(Time.infinityChallengeSum.totalSeconds, 0.1))),
     cap: DC.D2P30D0_61,
     formatEffect: value => formatX(value, 2, 1)
   },
