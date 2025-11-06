70eafa9d4d1b1a0e0edd8ce799ec9c55dc2d271f
Supersonic Seven <b.rockholz@icloud.com>
2025-11-06 15:32:43 -0500

    Update endgame-milestones.js
    That should be all

--- src/core/secret-formula/endgame/endgame-milestones.js
+++ src/core/secret-formula/endgame/endgame-milestones.js
@@ -2,17 +2,28 @@ export const endgameMilestones = {
   riftFill: {
     endgames: 1,
     reward: () => {
-      return `Rift Fill is ${formatPercents(0.05)} faster per Endgame, capping at ${formatPercents(0.5)} at ${formatInt(9)} Endgames`;
+      return `Rift Fill is ${formatPercents(0.05)} faster per Endgame, capping at ${formatPercents(0.5)} at ${formatInt(9)} Endgames` + 
+        (player.endgames >= 1
+         ? `(Currently: ${format(Math.min(0.45, player.endgames * 0.05), 2, 2)})`
+         : "(You have not yet reached this milestone)");
     }
   },
   remnantGalaxy: {
     endgames: 2,
-    reward: "You gain a multiplier to Galaxy strength based on Remnants"
+    reward: () => {
+      return "You gain a multiplier to Galaxy strength based on Remnants" +
+        (player.endgames >= 2 && Pelle.isDoomed
+         ? `(Currently: ${formatPercents(Math.pow(1 + Math.log10(Currency.remnants.value + 1), 0.5), 2, 2)})`
+         : (player.endgames < 2 ? "(You have not yet reached this milestone)" : "(Currently has no effect)"));
+    }
   },
   galGenAnimation: {
     endgames: 5,
     reward: () => {
-      return `Galaxy Generator Animations are ${formatX(1.2, 0, 1)} faster every ${formatInt(5)} Endgames, capping after ${formatInt(100)} Endgames`;
+      return `Galaxy Generator Animations are ${formatX(1.2, 0, 1)} faster every ${formatInt(5)} Endgames, capping after ${formatInt(100)} Endgames` + 
+        (player.endgames >= 5
+         ? `(Currently: ${format(Math.pow(1.2, Math.floor(Currency.endgames.value / 5)), 2, 2)})`
+         : "(You have not yet reached this milestone)");
     }
   },
   gameSpeedUncap: {
@@ -27,7 +38,7 @@ export const endgameMilestones = {
   },
   remnantFormula: {
     endgames: 50,
-    reward: "Improve the Remnant Formula"
+    reward: "Improve the Remnant Formula (see Remnant Gain Factors in the Pelle subtab)"
   },
   celestialEarlyUnlock: {
     endgames: 100,
@@ -37,11 +48,21 @@ export const endgameMilestones = {
   },
   moreFasterGalaxies: {
     endgames: 200,
-    reward: "Endgames boost Galaxy Production in Pelle"
+    reward: () => {
+      return "Endgames boost Galaxy Production in Pelle" + 
+        (player.endgames >= 200
+         ? `(Currently: ${format(Math.pow(10, Math.min(Currency.endgames.value / 100, 100)) * Math.pow(10, Math.max((Math.log10(Currency.endgames.value + 1) - 4) * 100, 0)), 2, 2)})`
+         : "(You have not yet reached this milestone)");
+    }
   },
   realityShardDTBoost: {
     endgames: 500,
-    reward: "Dilated Time gain is multiplied by your Reality Shard count"
+    reward: () => {
+      return "Dilated Time gain is multiplied by your Reality Shard count" + 
+        (player.endgames >= 500
+         ? `(Currently: ${format(Currency.realityShards.value.plus(1), 2, 2)})`
+         : "(You have not yet reached this milestone)");
+    }
   },
   autobuyerEndgame: {
     endgames: 1000,
@@ -49,10 +70,20 @@ export const endgameMilestones = {
   },
   endgameAntimatter: {
     endgames: 10000,
-    reward: "Gain a power to Antimatter Production based on Endgames, which is stronger in Pelle"
+    reward: () => {
+      return "Gain a power to Antimatter Production based on Endgames, which is stronger in Pelle" + 
+        (player.endgames >= 10000
+         ? `(Currently: ${formatPow(Pelle.isDoomed ? 1 + (Math.log10(Currency.endgames.value + 1) / 80) : 1 + (Math.log10(Currency.endgames.value + 1) / 200), 2, 3)})`
+         : "(You have not yet reached this milestone)");
+    }
   },
   instabilityReduction: {
     endgames: 1000000,
-    reward: "Endgames decrease the Galaxy Generator Instability Magnitude"
+    reward: () => {
+      return "Endgames decrease the Galaxy Generator Instability Magnitude" + 
+        (player.endgames >= 1000000
+         ? `(Currently: ${formatPow(Math.pow(1 / Math.log10(Currency.endgames.value + 1), 0.1), 2, 3)})`
+         : "(You have not yet reached this milestone)");
+    }
   }
 };
