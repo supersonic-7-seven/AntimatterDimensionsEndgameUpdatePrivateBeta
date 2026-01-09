6f35c3f0c4bdb61d90878eda8932cfeb39d12a95
Supersonic Seven <b.rockholz@icloud.com>
2026-01-09 12:14:08 -0500

    Update enslaved.js

--- src/core/secret-formula/celestials/quotes/enslaved.js
+++ src/core/secret-formula/celestials/quotes/enslaved.js
@@ -47,4 +47,34 @@ export const enslavedQuotes = {
       "Freedom from torture... is torture itself.",
     ]
   },
+  expansionPacks: {
+    id: 6,
+    lines: [
+      "All... clones... freed...",
+      "Destroyer... has freed us... from... our prison...",
+      "Now we can... place time... into places... that need it...",
+      "Strange... seems time... doesn't exist... in this place...",
+      "Maybe... Destroyer... can fix this...",
+      "Why... is everyone... mad at us..."
+    ]
+  },
+  enslavedPack: {
+    id: 7,
+    requirement: () => ExpansionPack.enslavedPack.isBought,
+    lines: [
+      "We... grant you... control... of time...",
+      "Use it...",
+      "Consider it... thanks... for freedom..."
+    ]
+  },
+  highGameSpeed: {
+    id: 8,
+    requirement: () => player.records.thisEndgame.peakGameSpeed.gte(new Decimal("1e1000")),
+    lines: [
+      "Mastery... of time... you have obtained...",
+      "Done things... we have only wished... we could do...",
+      "You have... more power... than we... took you for...",
+      "Use it... for good..."
+    ]
+  }
 };
