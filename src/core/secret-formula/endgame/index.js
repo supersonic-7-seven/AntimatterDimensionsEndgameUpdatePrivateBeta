5abaedb17609c875a7c328cc39a22ebced136f5e
Supersonic Seven <b.rockholz@icloud.com>
2026-01-09 12:03:38 -0500

    Update index.js

--- src/core/secret-formula/endgame/index.js
+++ src/core/secret-formula/endgame/index.js
@@ -2,10 +2,16 @@ import { breakEternityUpgrades } from "./break-eternity-upgrades";
 import { endgameMasteries } from "./endgame-masteries";
 import { permanentEndgameMasteries } from "./permanent-endgame-masteries";
 import { endgameMilestones } from "./endgame-milestones";
+import { endgameUpgrades } from "./endgame-upgrades";
+import { galacticPowerRewards } from "./galactic-power";
+import { expansionPacks } from "./expansion-packs";
 
 export const endgame = {
-  upgrades: breakEternityUpgrades,
+  breakUpgrades: breakEternityUpgrades,
   masteries: endgameMasteries,
   permanentMasteries: permanentEndgameMasteries,
-  milestones: endgameMilestones
+  milestones: endgameMilestones,
+  upgrades: endgameUpgrades,
+  galacticPowers: galacticPowerRewards,
+  packs: expansionPacks
 };
