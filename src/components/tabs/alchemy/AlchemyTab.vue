e7000730bb1283ffa003a6cb408f9a9f150fe79e
Supersonic Seven <b.rockholz@icloud.com>
2026-01-08 19:51:40 -0500

    Update AlchemyTab.vue

--- src/components/tabs/alchemy/AlchemyTab.vue
+++ src/components/tabs/alchemy/AlchemyTab.vue
@@ -63,7 +63,7 @@ export default {
   methods: {
     update() {
       this.reactionsAvailable = AlchemyResources.all.filter(res => !res.isBaseResource && res.isUnlocked).length !== 0;
-      this.realityCreationVisible = Ra.pets.effarig.level === 25;
+      this.realityCreationVisible = Ra.pets.effarig.level >= 25;
       this.animationTimer += 35;
       this.alchemyCap = Ra.alchemyResourceCap;
       this.capFactor = 1 / GlyphSacrificeHandler.glyphRefinementEfficiency;
@@ -182,6 +182,9 @@ export default {
         reaction.isActive = setIsActive;
       }
     },
+    resetAlchemy() {
+      for (const res of AlchemyResources.all) res.amount = 0;
+    },
     nodeClass(node) {
       const resource = node.resource;
       return {
@@ -214,6 +217,12 @@ export default {
         onclick="Modal.realityGlyph.show()"
       >
         View Reality Glyph creation
+      </PrimaryButton>
+      <PrimaryButton
+        class="o-primary-btn--subtab-option"
+        @click="resetAlchemy"
+      >
+        Force-reset all Alchemy resources
       </PrimaryButton>
     </div>
     <AlchemyResourceInfo
