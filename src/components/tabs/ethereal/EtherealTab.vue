a88b4df328ed47e8674e92858488c1045f538e8a
Supersonic Seven <b.rockholz@icloud.com>
2026-01-09 09:56:31 -0500

    Create EtherealTab.vue

---
+++ src/components/tabs/ethereal/EtherealTab.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "EtherealTab"
+};
+</script>
+
+<template>
+  <div>
+    <div class="nyi-ethereal-text">
+      <br>
+      <div>
+      <b>
+        The Ethereal
+      </b>
+      </div>
+      <br>
+      <div>
+        Coming in v0.3.1
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nyi-ethereal-text {
+  font-size: 2rem;
+  color: #0000c0;
+}
+</style>
