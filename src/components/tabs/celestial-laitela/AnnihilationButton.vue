118270e0a53ea330dec4a716930f63e951041af1
Supersonic Seven <b.rockholz@icloud.com>
2026-01-09 09:36:52 -0500

    Update AnnihilationButton.vue

--- src/components/tabs/celestial-laitela/AnnihilationButton.vue
+++ src/components/tabs/celestial-laitela/AnnihilationButton.vue
@@ -4,14 +4,17 @@ export default {
   data() {
     return {
       darkMatter: new Decimal(0),
-      darkMatterMult: 0,
-      darkMatterMultGain: 0,
+      darkMatterMult: new Decimal(0),
+      darkMatterMultGain: new Decimal(0),
       autobuyerUnlocked: false,
       annihilationButtonVisible: false,
       matterRequirement: 0,
-      darkMatterMultRatio: 0,
+      darkMatterMultRatio: new Decimal(0),
       autoAnnihilationInput: player.auto.annihilation.multiplier,
       isEnabled: true,
+      modeUnlocked: false,
+      annihilationMode: 0,
+      isBasic: true,
     };
   },
   computed: {
@@ -22,16 +25,22 @@ export default {
   methods: {
     update() {
       this.darkMatter.copyFrom(Currency.darkMatter);
-      this.darkMatterMult = Laitela.darkMatterMult;
-      this.darkMatterMultGain = Laitela.darkMatterMultGain;
+      this.darkMatterMult.copyFrom(Laitela.darkMatterMult);
+      this.darkMatterMultGain.copyFrom(Laitela.darkMatterMultGain);
       this.autobuyerUnlocked = Autobuyer.annihilation.isUnlocked;
       this.annihilationButtonVisible = Laitela.canAnnihilate || this.autobuyerUnlocked;
       this.matterRequirement = Laitela.annihilationDMRequirement;
-      this.darkMatterMultRatio = Laitela.darkMatterMultRatio;
+      this.darkMatterMultRatio.copyFrom(Laitela.darkMatterMultRatio);
       this.isEnabled = player.auto.annihilation.isActive;
+      this.modeUnlocked = ExpansionPack.laitelaPack.isBought;
+      this.annihilationMode = player.auto.annihilation.mode;
+      this.isBasic = this.annihilationMode === 0;
     },
     annihilate() {
       Laitela.annihilate();
+    },
+    modeToggle() {
+      player.auto.annihilation.mode = (player.auto.annihilation.mode + 1) % 2;
     },
     handleAutoAnnihilationInputChange() {
       const float = parseFloat(this.autoAnnihilationInput);
@@ -40,13 +49,19 @@ export default {
       } else {
         player.auto.annihilation.multiplier = float;
       }
+    },
+    classObject() {
+      return {
+        "l-laitela-annihilation-container": true,
+        "l-laitela-annihilation-container--large": this.modeUnlocked
+      };
     }
   }
 };
 </script>
 
 <template>
-  <div class="l-laitela-annihilation-container">
+  <div :class="classObject()">
     <button
       v-if="darkMatter.lt(matterRequirement)"
       class="l-laitela-annihilation-button"
@@ -62,7 +77,7 @@ export default {
     </button>
     <br>
     <br>
-    <span v-if="darkMatterMult > 1">
+    <span v-if="darkMatterMult.gt(1)">
       Current multiplier to all Dark Matter Dimensions: <b>{{ formatX(darkMatterMult, 2, 2) }}</b>
       <br>
       <br>
@@ -73,7 +88,12 @@ export default {
       <span v-if="autobuyerUnlocked">
         <br>
         <br>
-        Auto-Annihilate when adding
+        <span v-if="isBasic">
+          Auto-Annihilate when adding
+        </span>
+        <span v-if="!isBasic">
+          Auto-Annihilate when the pending multiplier is
+        </span>
         <input
           v-model="autoAnnihilationInput"
           type="text"
@@ -81,12 +101,26 @@ export default {
           class="c-small-autobuyer-input c-laitela-annihilation-input"
           @change="handleAutoAnnihilationInputChange()"
         >
-        to the multiplier.
+        <span v-if="isBasic">
+          to the multiplier.
+        </span>
+        <span v-if="!isBasic">
+          times higher than the current multiplier.
+        </span>
       </span>
     </span>
     <span v-else>
       Annihilation will reset your Dark Matter and Dark Matter Dimension amounts, but will give a permanent
-      multiplier of <b>{{ formatX(1 + darkMatterMultGain, 2, 2) }}</b> to all Dark Matter Dimensions.
+      multiplier of <b>{{ formatX(darkMatterMultGain.add(1), 2, 2) }}</b> to all Dark Matter Dimensions.
     </span>
+    <br>
+    <br>
+    <button
+      v-if="modeUnlocked"
+      class="l-laitela-annihilation-button c-laitela-annihilation-button"
+      @click="modeToggle"
+    >
+      <b>Toggle Autobuyer Mode</b>
+    </button>
   </div>
 </template>
