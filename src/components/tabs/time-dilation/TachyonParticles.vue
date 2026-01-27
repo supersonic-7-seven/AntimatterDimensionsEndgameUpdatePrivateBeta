452cb545ee55154c9723052cf2f083e4e3314359
Supersonic Seven <b.rockholz@icloud.com>
2026-01-27 17:24:16 -0500

    Update TachyonParticles.vue

--- src/components/tabs/time-dilation/TachyonParticles.vue
+++ src/components/tabs/time-dilation/TachyonParticles.vue
@@ -28,7 +28,7 @@ export default {
   methods: {
     update() {
       this.count = Currency.tachyonParticles.gte(1)
-        ? Math.clampMin(Math.floor(20 * Math.log10(Currency.tachyonParticles.exponent)), 1)
+        ? Decimal.clampMin(Decimal.floor(Decimal.log10(Currency.tachyonParticles.value.add(10).log10()).times(20)), 1).toNumber()
         : 0;
       this.isBlob = Theme.currentName() === "S11";
     },
