036a2882ef7edde995a1469517ec4f30af308424
Supersonic Seven <b.rockholz@icloud.com>
2026-01-27 18:55:52 -0500

    Create alpha.js

---
+++ src/core/secret-formula/celestials/quotes/alpha.js
@@ -0,0 +1,28 @@
+export const alphaQuotes = {
+  unlock: {
+    id: 0,
+    lines: [
+      "I sense...",
+      "Someone has arrived...",
+      "Hello?",
+      "Wait...",
+      "You're a mortal?",
+      "...",
+      "How can a mortal like you ever survive the ascent up to me?",
+      "To face true Celestial power?",
+      "You were helped, weren't you?",
+      "By the lesser Celestials, right?",
+      "And Pelle?",
+      "Should've known.",
+      "Pelle's never followed orders since I sent him out to recruit other Celestials.",
+      "He thinks he's the leader now.",
+      "I'll smite him soon enough.",
+      "For now, I must deal with you.",
+      "Go ahead, enter my domain.",
+      "But know this.",
+      "There is no escape.",
+      "For I am Alpha, Celestial of Darkness.",
+      "And you will never see the light again."
+    ]
+  }
+};
