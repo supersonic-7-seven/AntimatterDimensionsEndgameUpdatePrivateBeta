9ceb215dfd8629839ef1ee20fcf75010c92cfe1c
Supersonic Seven <b.rockholz@icloud.com>
2026-01-27 17:11:33 -0500

    Update ResetRealityButton.vue

--- src/components/tabs/glyphs/ResetRealityButton.vue
+++ src/components/tabs/glyphs/ResetRealityButton.vue
@@ -19,7 +19,7 @@ export default {
   },
   methods: {
     update() {
-      this.canReality = TimeStudy.reality.isBought && player.records.thisReality.maxEP.exponent >= 4000;
+      this.canReality = TimeStudy.reality.isBought && player.records.thisReality.maxEP.add(1).log10().gte(4000);
       this.resetCelestial = player.options.retryCelestial;
       this.isInCelestialReality = isInCelestialReality();
       this.isDoomed = Pelle.isDoomed;
