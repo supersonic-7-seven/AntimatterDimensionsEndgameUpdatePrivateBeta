5340b807b779de54918dc0716155baf6c3165a97
Supersonic Seven <b.rockholz@icloud.com>
2026-01-27 16:28:47 -0500

    Update EternityButton.vue

--- src/components/ui-modes/prestige-header/EternityButton.vue
+++ src/components/ui-modes/prestige-header/EternityButton.vue
@@ -60,15 +60,15 @@ export default {
         ],
         [0, 255, 0]
       ];
-      const ratio = this.gainedEP.log10() / this.currentEP.log10();
+      const ratio = this.gainedEP.log10().div(this.currentEP.log10());
       const interFn = index => {
-        if (ratio < 0.9) return stepRGB[0][index];
-        if (ratio < 1) {
-          const r = 10 * (ratio - 0.9);
+        if (ratio.lt(0.9)) return stepRGB[0][index];
+        if (ratio.lt(1)) {
+          const r = (ratio.sub(0.9)).times(10).toNumber();
           return Math.round(stepRGB[0][index] * (1 - r) + stepRGB[1][index] * r);
         }
-        if (ratio < 1.1) {
-          const r = 10 * (ratio - 1);
+        if (ratio.lt(1.1)) {
+          const r = (ratio.sub(1)).times(10).toNumber();
           return Math.round(stepRGB[1][index] * (1 - r) + stepRGB[2][index] * r);
         }
         return stepRGB[2][index];
@@ -138,7 +138,7 @@ export default {
       this.currentEP.copyFrom(Currency.eternityPoints);
       this.gainedEP.copyFrom(gainedEP);
       const hasNewContent = !PlayerProgress.realityUnlocked() &&
-        Currency.eternityPoints.exponent >= 4000 &&
+        Currency.eternityPoints.value.add(1).log10().gte(4000) &&
         !TimeStudy.reality.isBought;
       if (this.isDilation) {
         this.type = hasNewContent
