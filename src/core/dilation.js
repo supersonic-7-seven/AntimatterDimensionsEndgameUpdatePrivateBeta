1ab0ef8c859bf0b05a0739eff8f3dbfafd29f8bb
Supersonic Seven <b.rockholz@icloud.com>
2026-01-28 09:56:58 -0500

    Update dilation.js

--- src/core/dilation.js
+++ src/core/dilation.js
@@ -272,7 +272,8 @@ export function getDilationTimeEstimate(goal) {
 }
 
 export function dilatedValueOf(value) {
-  const log10 = value.max(1).log10();
+  if (value.lte(0)) return 0;
+  const log10 = value.log10();
   const dilationPenalty = 0.75 * Effects.product(DilationUpgrade.dilationPenalty);
   return Decimal.pow10(new Decimal(Decimal.sign(log10)).times(Decimal.pow(Decimal.abs(log10), dilationPenalty)));
 }
